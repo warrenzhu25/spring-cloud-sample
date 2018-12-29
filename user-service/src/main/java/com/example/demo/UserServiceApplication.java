0e614a60db00decb65bcd804e1bfcc98c485b920
Warren Zhu <zhonzh@microsoft.com>
2018-12-29 11:01:08 +0800

    Make user service support zipkin and eureka

--- user-service/src/main/java/com/example/demo/UserServiceApplication.java
+++ user-service/src/main/java/com/example/demo/UserServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@EnableCaching
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class UserServiceApplication {
 
     public static void main(String[] args) {
